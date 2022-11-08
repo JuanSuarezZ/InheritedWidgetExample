3af065db1d3f24afb262e88fda35185fc422015d
juan suarez <juansebastiansuarez28@gmail.com>
2022-11-07 22:26:39 -0500

    first

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Titile Material App',
       home: Home(),
       routes: {'pag2': (_) => Pag2()},
     );
